9cb0afef1797e8cfd46c5872e1c0153f9ef4dc75
simanzler <simanzler@gmail.com>
2024-04-02 16:24:22 -0400

    Initial Commit

---
+++ app/(modals)/login.tsx
@@ -0,0 +1,171 @@
+import Colors from '@/constants/Colors';
+import { useOAuth, useSignIn } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import { View, StyleSheet, TextInput, Text, TouchableOpacity } from 'react-native';
+
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
+import { defaultStyles } from '@/constants/Styles';
+import { useState } from 'react';
+
+enum Strategy {
+  Google = 'oauth_google',
+  Apple = 'oauth_apple',
+  Facebook = 'oauth_facebook',
+}
+const Page = () => {
+  useWarmUpBrowser();
+
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onSignInPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const completeSignIn = await signIn.create({
+        identifier: emailAddress,
+        password,
+      });
+
+      // This indicates the user is signed in
+      await setActive({ session: completeSignIn.createdSessionId });
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // const router = useRouter();
+  // const { startOAuthFlow: googleAuth } = useOAuth({ strategy: 'oauth_google' });
+  // const { startOAuthFlow: appleAuth } = useOAuth({ strategy: 'oauth_apple' });
+  // const { startOAuthFlow: facebookAuth } = useOAuth({ strategy: 'oauth_facebook' });
+
+  // const onSelectAuth = async (strategy: Strategy) => {
+  //   const selectedAuth = {
+  //     [Strategy.Google]: googleAuth,
+  //     [Strategy.Apple]: appleAuth,
+  //     [Strategy.Facebook]: facebookAuth,
+  //   }[strategy];
+
+  //   try {
+  //     const { createdSessionId, setActive } = await selectedAuth();
+  //     console.log("SessionID: " + createdSessionId);
+
+  //     if (createdSessionId) {
+  //       console.log("yo");
+  //       setActive!({ session: createdSessionId });
+  //       router.back();
+  //     }
+  //   } catch (err: any) {
+  //     console.log('OAuth error'+ err);
+  //   }
+  // };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        value={emailAddress}
+        onChangeText={setEmailAddress}
+        style={[defaultStyles.inputField, { marginBottom: 10 }]}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+
+      <TouchableOpacity style={defaultStyles.btn} onPress={onSignInPress}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.seperatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.seperator}>or</Text>
+        {/* <View
+          style={{
+            flex: 1,
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        /> */}
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="mail-outline" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="logo-apple" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="logo-google" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="logo-facebook" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 26,
+  },
+
+  seperatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  },
+});
